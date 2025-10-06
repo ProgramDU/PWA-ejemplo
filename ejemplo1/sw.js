5bea06173ac74573a31a39704e4e80c17b1b14bc
ProgramDU <bernardinodaniela96@gmail.com>
2025-10-06 13:04:30 -0600

    first commit

---
+++ ejemplo1/sw.js
@@ -0,0 +1,59 @@
+//1. Nombre del service worker y los archivos a cachear
+const CACHE_NAME = "mi-cache";
+const BASE_PATH = "PWA-ejemplo/" //sustituir nombre real de la PWA
+const urlsToCache = [//lista de archivos a cachear
+    `${BASE_PATH}index.html`,
+    `${BASE_PATH}manifest.json`,
+    `${BASE_PATH}style.css`,
+    `${BASE_PATH}offline.html`,
+    `${BASE_PATH}icons/icon-192x192.png`,
+    `${BASE_PATH}icons/icon-512x512.png`,
+
+];
+
+//2. Evento INSTALL -> se ejecuta al instalar el servie worker, se cachean
+// (se meten a cache) los recursos base de la PWA
+self.addEventListener("install", event => {
+    console.log("Service Worker: Instalando...");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Archivos cacheados");
+            cache.addAll(urlsToCache)
+        })
+    );
+});
+
+//3. Evento ACTIVATE -> se ejecuta al activar el service worker y necesito limpiar
+// caches antiguos, para mantener version actual 
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+//4. Evento FETCH -> intercepta peticiones de la app 
+// Intercepta cada peticion de la PWA. 
+// Busca primero en cache y si no esta busca en internet
+// En caso de falla muestra la pagina offline.html
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch (() => caches.match("offline.html"))
+        })
+    );
+}); 
+
+//5. Evento PUSH -> notificaciones en segundo plano
+// manejo de notificaciones push (opcioonnal)
+self.addEventListener("push", event => {
+    const data = event.data ? event.data.text() : "Notificacion sin texto";
+    event.waitUntil(
+        self.registration.showNotification("Mi PWA" , {body: data})
+    );
+});
+//
